6a0323af764dd0ec7b4f564712d25efaa06ab93e
Aego <aego.yu@gmail.com>
2019-11-18 15:51:59 -0800

    switched to use yarn

---
+++ assets/lottie/list_loading.d.ts
@@ -0,0 +1,318 @@
+export declare const v: string;
+export declare const fr: number;
+export declare const ip: number;
+export declare const op: number;
+export declare const w: number;
+export declare const h: number;
+export declare const nm: string;
+export declare const ddd: number;
+export declare const assets: {
+    "id": string;
+    "layers": {
+        "ddd": number;
+        "ind": number;
+        "ty": number;
+        "nm": string;
+        "sr": number;
+        "ks": {
+            "o": {
+                "a": number;
+                "k": number;
+                "ix": number;
+            };
+            "r": {
+                "a": number;
+                "k": number;
+                "ix": number;
+            };
+            "p": {
+                "a": number;
+                "k": number[];
+                "ix": number;
+            };
+            "a": {
+                "a": number;
+                "k": number[];
+                "ix": number;
+            };
+            "s": {
+                "a": number;
+                "k": ({
+                    "i": {
+                        "x": number[];
+                        "y": number[];
+                    };
+                    "o": {
+                        "x": number[];
+                        "y": number[];
+                    };
+                    "n": string[];
+                    "t": number;
+                    "s": number[];
+                    "e": number[];
+                } | {
+                    "t": number;
+                    "i"?: undefined;
+                    "o"?: undefined;
+                    "n"?: undefined;
+                    "s"?: undefined;
+                    "e"?: undefined;
+                })[];
+                "ix": number;
+            };
+        };
+        "ao": number;
+        "shapes": {
+            "ty": string;
+            "it": ({
+                "ty": string;
+                "d": number;
+                "s": {
+                    "a": number;
+                    "k": number[];
+                    "ix": number;
+                };
+                "p": {
+                    "a": number;
+                    "k": number[];
+                    "ix": number;
+                };
+                "r": {
+                    "a": number;
+                    "k": number;
+                    "ix": number;
+                };
+                "nm": string;
+                "mn": string;
+                "hd": boolean;
+                "c"?: undefined;
+                "o"?: undefined;
+                "a"?: undefined;
+                "sk"?: undefined;
+                "sa"?: undefined;
+            } | {
+                "ty": string;
+                "c": {
+                    "a": number;
+                    "k": number[];
+                    "ix": number;
+                };
+                "o": {
+                    "a": number;
+                    "k": number;
+                    "ix": number;
+                };
+                "r": number;
+                "nm": string;
+                "mn": string;
+                "hd": boolean;
+                "d"?: undefined;
+                "s"?: undefined;
+                "p"?: undefined;
+                "a"?: undefined;
+                "sk"?: undefined;
+                "sa"?: undefined;
+            } | {
+                "ty": string;
+                "p": {
+                    "a": number;
+                    "k": number[];
+                    "ix": number;
+                };
+                "a": {
+                    "a": number;
+                    "k": number[];
+                    "ix": number;
+                };
+                "s": {
+                    "a": number;
+                    "k": number[];
+                    "ix": number;
+                };
+                "r": {
+                    "a": number;
+                    "k": number;
+                    "ix": number;
+                };
+                "o": {
+                    "a": number;
+                    "k": number;
+                    "ix": number;
+                };
+                "sk": {
+                    "a": number;
+                    "k": number;
+                    "ix": number;
+                };
+                "sa": {
+                    "a": number;
+                    "k": number;
+                    "ix": number;
+                };
+                "nm": string;
+                "d"?: undefined;
+                "mn"?: undefined;
+                "hd"?: undefined;
+                "c"?: undefined;
+            })[];
+            "nm": string;
+            "np": number;
+            "cix": number;
+            "ix": number;
+            "mn": string;
+            "hd": boolean;
+        }[];
+        "ip": number;
+        "op": number;
+        "st": number;
+        "bm": number;
+    }[];
+}[];
+export declare const layers: ({
+    "ddd": number;
+    "ind": number;
+    "ty": number;
+    "nm": string;
+    "refId": string;
+    "sr": number;
+    "ks": {
+        "o": {
+            "a": number;
+            "k": number;
+            "ix": number;
+        };
+        "r": {
+            "a": number;
+            "k": number;
+            "ix": number;
+        };
+        "p": {
+            "a": number;
+            "k": number[];
+            "ix": number;
+        };
+        "a": {
+            "a": number;
+            "k": number[];
+            "ix": number;
+        };
+        "s": {
+            "a": number;
+            "k": number[];
+            "ix": number;
+        };
+    };
+    "ao": number;
+    "w": number;
+    "h": number;
+    "ip": number;
+    "op": number;
+    "st": number;
+    "bm": number;
+} | {
+    "ddd": number;
+    "ind": number;
+    "ty": number;
+    "nm": string;
+    "refId": string;
+    "sr": number;
+    "ks": {
+        "o": {
+            "a": number;
+            "k": ({
+                "i": {
+                    "x": number[];
+                    "y": number[];
+                };
+                "o": {
+                    "x": number[];
+                    "y": number[];
+                };
+                "n": string[];
+                "t": number;
+                "s": number[];
+                "e": number[];
+            } | {
+                "t": number;
+                "i"?: undefined;
+                "o"?: undefined;
+                "n"?: undefined;
+                "s"?: undefined;
+                "e"?: undefined;
+            })[];
+            "ix": number;
+        };
+        "r": {
+            "a": number;
+            "k": number;
+            "ix": number;
+        };
+        "p": {
+            "a": number;
+            "k": ({
+                "i": {
+                    "x": number;
+                    "y": number;
+                };
+                "o": {
+                    "x": number;
+                    "y": number;
+                };
+                "n": string;
+                "t": number;
+                "s": number[];
+                "e": number[];
+                "to": number[];
+                "ti": number[];
+            } | {
+                "t": number;
+                "i"?: undefined;
+                "o"?: undefined;
+                "n"?: undefined;
+                "s"?: undefined;
+                "e"?: undefined;
+                "to"?: undefined;
+                "ti"?: undefined;
+            })[];
+            "ix": number;
+        };
+        "a": {
+            "a": number;
+            "k": number[];
+            "ix": number;
+        };
+        "s": {
+            "a": number;
+            "k": ({
+                "i": {
+                    "x": number[];
+                    "y": number[];
+                };
+                "o": {
+                    "x": number[];
+                    "y": number[];
+                };
+                "n": string[];
+                "t": number;
+                "s": number[];
+                "e": number[];
+            } | {
+                "t": number;
+                "i"?: undefined;
+                "o"?: undefined;
+                "n"?: undefined;
+                "s"?: undefined;
+                "e"?: undefined;
+            })[];
+            "ix": number;
+        };
+    };
+    "ao": number;
+    "w": number;
+    "h": number;
+    "ip": number;
+    "op": number;
+    "st": number;
+    "bm": number;
+})[];
+export declare const markers: any[];
---
+++ assets/lottie/loading.d.ts
@@ -0,0 +1,250 @@
+export declare const v: string;
+export declare const fr: number;
+export declare const ip: number;
+export declare const op: number;
+export declare const w: number;
+export declare const h: number;
+export declare const nm: string;
+export declare const ddd: number;
+export declare const assets: any[];
+export declare const layers: {
+    "ddd": number;
+    "ind": number;
+    "ty": number;
+    "nm": string;
+    "ks": {
+        "o": {
+            "a": number;
+            "k": number;
+        };
+        "r": {
+            "a": number;
+            "k": number;
+        };
+        "p": {
+            "a": number;
+            "k": number[];
+        };
+        "a": {
+            "a": number;
+            "k": number[];
+        };
+        "s": {
+            "a": number;
+            "k": number[];
+        };
+    };
+    "ao": number;
+    "shapes": {
+        "ty": string;
+        "it": ({
+            "d": number;
+            "ty": string;
+            "s": {
+                "a": number;
+                "k": number[];
+                "ix"?: undefined;
+            };
+            "p": {
+                "a": number;
+                "k": number[];
+                "ix"?: undefined;
+            };
+            "nm": string;
+            "mn": string;
+            "e"?: undefined;
+            "o"?: undefined;
+            "m"?: undefined;
+            "ix"?: undefined;
+            "c"?: undefined;
+            "w"?: undefined;
+            "lc"?: undefined;
+            "lj"?: undefined;
+            "ml"?: undefined;
+            "a"?: undefined;
+            "r"?: undefined;
+            "sk"?: undefined;
+            "sa"?: undefined;
+        } | {
+            "ty": string;
+            "s": {
+                "a": number;
+                "k": ({
+                    "i": {
+                        "x": number[];
+                        "y": number[];
+                    };
+                    "o": {
+                        "x": number[];
+                        "y": number[];
+                    };
+                    "n": string[];
+                    "t": number;
+                    "s": number[];
+                    "e": number[];
+                } | {
+                    "t": number;
+                    "i"?: undefined;
+                    "o"?: undefined;
+                    "n"?: undefined;
+                    "s"?: undefined;
+                    "e"?: undefined;
+                })[];
+                "ix": number;
+            };
+            "e": {
+                "a": number;
+                "k": ({
+                    "i": {
+                        "x": number[];
+                        "y": number[];
+                    };
+                    "o": {
+                        "x": number[];
+                        "y": number[];
+                    };
+                    "n": string[];
+                    "t": number;
+                    "s": number[];
+                    "e": number[];
+                } | {
+                    "t": number;
+                    "i"?: undefined;
+                    "o"?: undefined;
+                    "n"?: undefined;
+                    "s"?: undefined;
+                    "e"?: undefined;
+                })[];
+                "ix": number;
+            };
+            "o": {
+                "a": number;
+                "k": ({
+                    "i": {
+                        "x": number[];
+                        "y": number[];
+                    };
+                    "o": {
+                        "x": number[];
+                        "y": number[];
+                    };
+                    "n": string[];
+                    "t": number;
+                    "s": number[];
+                    "e": number[];
+                } | {
+                    "t": number;
+                    "i"?: undefined;
+                    "o"?: undefined;
+                    "n"?: undefined;
+                    "s"?: undefined;
+                    "e"?: undefined;
+                })[];
+                "ix": number;
+            };
+            "m": number;
+            "ix": number;
+            "nm": string;
+            "mn": string;
+            "d"?: undefined;
+            "p"?: undefined;
+            "c"?: undefined;
+            "w"?: undefined;
+            "lc"?: undefined;
+            "lj"?: undefined;
+            "ml"?: undefined;
+            "a"?: undefined;
+            "r"?: undefined;
+            "sk"?: undefined;
+            "sa"?: undefined;
+        } | {
+            "ty": string;
+            "c": {
+                "a": number;
+                "k": number[];
+            };
+            "o": {
+                "a": number;
+                "k": number;
+                "ix"?: undefined;
+            };
+            "w": {
+                "a": number;
+                "k": number;
+            };
+            "lc": number;
+            "lj": number;
+            "ml": number;
+            "nm": string;
+            "mn": string;
+            "d"?: undefined;
+            "s"?: undefined;
+            "p"?: undefined;
+            "e"?: undefined;
+            "m"?: undefined;
+            "ix"?: undefined;
+            "a"?: undefined;
+            "r"?: undefined;
+            "sk"?: undefined;
+            "sa"?: undefined;
+        } | {
+            "ty": string;
+            "p": {
+                "a": number;
+                "k": number[];
+                "ix": number;
+            };
+            "a": {
+                "a": number;
+                "k": number[];
+                "ix": number;
+            };
+            "s": {
+                "a": number;
+                "k": number[];
+                "ix": number;
+            };
+            "r": {
+                "a": number;
+                "k": number;
+                "ix": number;
+            };
+            "o": {
+                "a": number;
+                "k": number;
+                "ix": number;
+            };
+            "sk": {
+                "a": number;
+                "k": number;
+                "ix": number;
+            };
+            "sa": {
+                "a": number;
+                "k": number;
+                "ix": number;
+            };
+            "nm": string;
+            "d"?: undefined;
+            "mn"?: undefined;
+            "e"?: undefined;
+            "m"?: undefined;
+            "ix"?: undefined;
+            "c"?: undefined;
+            "w"?: undefined;
+            "lc"?: undefined;
+            "lj"?: undefined;
+            "ml"?: undefined;
+        })[];
+        "nm": string;
+        "np": number;
+        "cix": number;
+        "ix": number;
+        "mn": string;
+    }[];
+    "ip": number;
+    "op": number;
+    "st": number;
+    "bm": number;
+    "sr": number;
+}[];
