228eb8965068c7653a5be150ba95c5ecf3c228ac
Aego <aego.yu@gmail.com>
2019-11-18 23:44:55 -0800

    added 'auto' for box

--- src/types/common-props.ts
+++ src/types/common-props.ts
@@ -1,17 +1,17 @@
 export interface BoxProps {
-  padding?: number
-  pt?: number
-  pb?: number
-  pl?: number
-  pr?: number
-  px?: number
-  py?: number
+  padding?: number | 'auto'
+  pt?: number | 'auto'
+  pb?: number | 'auto'
+  pl?: number | 'auto'
+  pr?: number | 'auto'
+  px?: number | 'auto'
+  py?: number | 'auto'
 
-  margin?: number
-  mt?: number
-  mb?: number
-  ml?: number
-  mr?: number
-  mx?: number
-  my?: number
+  margin?: number | 'auto'
+  mt?: number | 'auto'
+  mb?: number | 'auto'
+  ml?: number | 'auto'
+  mr?: number | 'auto'
+  mx?: number | 'auto'
+  my?: number | 'auto'
 }
